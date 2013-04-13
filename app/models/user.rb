1a7d46958a56f9afff477aa4b37274dcc6a990ec
Traz <a.reith@gmail.com>
2013-04-13 17:11:37 +1100

    update BS3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
  
-  validates :password,                presence: true, confirmation: true, length: { minimum: 3}
+  validates :password,                presence: true, confirmation: true, length: { minimum: 3}, :if => :password
   validates :email,                   presence: true, uniqueness: true
-  validates :password_confirmation,   presence: true
+  validates :password_confirmation,   presence: true, :if => :password
+  validates :username,                presence: true
 
 end
